cdc26606867f55ace0b23b85635a1a8e9ab09820
Lucas <lucashen@gmail.com>
2015-05-19 21:16:18 -0300

    include status 500

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ bookRouter.route('/Books')
     .get(function(req, res){
         Book.find(function(err, books){
             if(err)
-                console.log(err)
+                res.status(500).send(err);
             else
                 res.json(books);
         })
